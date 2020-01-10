e171a858c8525cd58e237965a2719c38bc04ad4e
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2020-01-09 22:22:30 -0500

    deleted old models

--- API/api.py
+++ API/api.py
@@ -14,8 +14,8 @@ def create_app():
         comment = from_backend['comment']
         # user_name = from_backend['user_name']
 
-        model = load_model('baseline_model_v2.h5.h5')
-        vectorizer = pickle.load(open('tfidf_v2.pickle', 'rb'))
+        model = load_model('API\\baseline_model_v2.h5.h5')
+        vectorizer = pickle.load(open('API\\tfidf_v2.pickle', 'rb'))
         predictions = model.predict(vectorizer.transform([comment]).toarray())
         predictions = predictions.tolist()
 
