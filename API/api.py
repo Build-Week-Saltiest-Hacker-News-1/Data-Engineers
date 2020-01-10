c316fff4386c20ac9b44e7b987bc6e12a1bdd25c
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2020-01-09 22:36:12 -0500

    filename

--- API/api.py
+++ API/api.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 import pickle 
 from sklearn.feature_extraction.text import TfidfVectorizer
+import os
+
+
+model = load_model('./API/baseline_model_v2.h5')
+vectorizer = pickle.load(open('API\\tfidf_v2.pickle', 'rb'))
 
 def create_app():
     app = Flask(__name__)
@@ -14,8 +19,8 @@ def create_app():
         comment = from_backend['comment']
         # user_name = from_backend['user_name']
 
-        model = load_model('API\\base_model')
-        vectorizer = pickle.load(open('API\\tfidf_v2.pickle', 'rb'))
+        # model = load_model('API\\base_model')
+        # vectorizer = pickle.load(open('API\\tfidf_v2.pickle', 'rb'))
         predictions = model.predict(vectorizer.transform([comment]).toarray())
         predictions = predictions.tolist()
 
